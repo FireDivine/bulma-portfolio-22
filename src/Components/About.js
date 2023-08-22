e58ceee760417a22dd049bf579520c4ddffb2f18
Jillian <firedivine46@gmail.com>
2023-08-22 13:23:59 +1200

    added some hobbies to about me page

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,6 +12,35 @@ function About() {
           around tech since my high school days and I highly enjoyed every
           second of it. I love to see my projects come alive as I code.
         </h2>
+        <br />
+        <br />
+        <h3 className="title is-3 is-uppercase">Some Hobbies</h3>
+        <h4 className="subtitle">
+          <ul>
+            <li>Amigurumi - the crochet of toys</li>
+            <img
+              style={{
+                'border-radius': '50%',
+                width: '15% !important',
+                padding: '5px',
+              }}
+              src="images/panda_front.jpg"
+              alt="panda boi front"
+            />
+            <img
+              style={{
+                'border-radius': '50%',
+                width: '15% !important',
+                padding: '5px',
+              }}
+              src="images/panda_side.jpg"
+              alt="panda boi side"
+            />
+            <li>Baking</li>
+            <li>movies</li>
+            <li>PC Gaming / hanging out with friends online</li>
+          </ul>
+        </h4>
       </section>
     </div>
   )
